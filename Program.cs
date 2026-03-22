d9d9a310d6b5fba857bb0da2c13d2f804d03d863
m1nato <m1nato@DESKTOP-B48COC8>
2026-03-22 16:34:43 +0200

    +

--- Program.cs
+++ Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace testpiu
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Introduceti id, denumire si pretul medicamentului:");
+            string idstring = Console.ReadLine();
+            int.TryParse(idstring, out int id);
+            string den = Console.ReadLine();
+            string pretstring = Console.ReadLine();
+            int.TryParse(pretstring, out int pret);
+            var med1 = new medicament(id, den, pret);
+            Console.WriteLine(med1.info());
+            Console.ReadKey();
+        }
+    }
+}
