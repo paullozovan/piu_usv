11db702c2c8a398718027917a3f417975c38c527
m1nato <m1nato@DESKTOP-B48COC8>
2026-04-02 18:49:10 +0300

    proiectul a fost initializat gresit)

---
+++ LibrarieModele/medicament.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibrarieModele
+{
+    public class medicament
+    {
+        [Flags]
+        public enum CategoriiMedicament
+        {
+            Analgezic = 1,      
+            Antipyretic = 2,    
+            Antiinflamator = 4, 
+            Antibiotic = 8,     
+            Antiviral = 16     
+        }
+        public enum ModEliberare
+        {
+            CuPrescriptie = 1,
+            FaraPrescriptie = 2
+        };
+        public ModEliberare me;
+        private const char SEPARATOR = ' ';
+        public int id { get; set; }
+        public string den { get; set; }
+        public int pret { get; set; }
+        public int opt { get; set; }
+        public string mod { get; set; }
+        public CategoriiMedicament categorii { get; set; }
+
+        public bool setopt(int opti)
+        {
+            if (opti == 1 || opti == 2)
+            {
+                opt = opti;
+                ModEliberare me = (ModEliberare)opt;
+                mod = me.ToString();
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("Optiune inexistenta, alegeti 1 sau 2!");
+                return false;
+            }
+        }
+        public bool setcat(string opti)
+        {
+            string[] optiuni = opti.Split(SEPARATOR);
+            foreach (string opt in optiuni)
+            {
+                if (Enum.TryParse(opt, true, out CategoriiMedicament categorie))
+                {
+                    if (!Enum.IsDefined(typeof(CategoriiMedicament), categorie))
+                    {
+                        return false;
+                    }
+                    categorii |= categorie;
+                }
+                else
+                {
+                    Console.WriteLine($"Numar invalid: {opt}");
+                    return false;
+                }
+            }
+            return true;
+        }
+        public medicament() 
+        {
+            id = 0;
+            den = string.Empty;
+            pret = 0;
+        }
+
+        public medicament(int id, string den, int pret)
+        {
+            this.id = id;
+            this.den = den;
+            this.pret = pret;
+        }
+
+        public string info()
+        {
+            string categoriiText = (categorii == 0) ? "Niciuna" : categorii.ToString();
+            return $@"--------------------------------------
+ID: {id}
+Denumire: {den}
+Pret: {pret} Lei
+Mod Eliberare: {mod}
+Categorii: {categoriiText}
+--------------------------------------";
+        }
+
+
+    }
+}
---
+++ NivelStocareDate/AdministrareMedicamente.cs
@@ -0,0 +1,66 @@
+﻿using LibrarieModele;
+namespace NivelStocareDate
+{
+    public class AdministrareMedicamente
+    {
+        public List<medicament> meds;
+
+        public AdministrareMedicamente()
+        {
+            meds = new List<medicament>();
+        }
+
+        public void AddStudent(medicament med)
+        {
+            med.id = GetNextIdMed();
+            meds.Add(med);
+        }
+
+        public List<medicament> GetMeds()
+        {
+            return meds;
+        }
+
+        public medicament? GetMed(int idMed)
+        {
+            foreach (medicament med in meds)
+            {
+                if (med.id == idMed)
+                {
+                    return med;
+                }
+            }
+
+            return null;
+        }
+
+        public medicament? GetMed(string _den)
+        {
+            return meds?.FirstOrDefault(med =>
+                med.den.Equals(_den, StringComparison.OrdinalIgnoreCase)
+            );
+        }
+        public List<medicament> GetMedsCat(medicament.CategoriiMedicament cat)
+        {
+            var medsel = from med in meds
+                         where (med.categorii & cat) == cat
+                         select med;
+            return medsel.ToList();
+        }
+
+        public bool UpdateMedicament(medicament s)
+        {
+            throw new Exception("Optiunea UpdateMedicament nu este implementata");
+        }
+
+        public int GetNextIdMed()
+        {
+            if (meds.Count == 0)
+            {
+                return 1;
+            }
+
+            return meds.Last().id + 1;
+        }
+    }
+}
